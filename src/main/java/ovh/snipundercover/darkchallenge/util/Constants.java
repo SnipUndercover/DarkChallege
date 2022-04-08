89b1135cf801993f83772d539552b296986b1bdf
SnipUndercover <mxxhxx11@gmail.com>
2022-04-08 02:23:53 +0200

    Optimize Constants#WATERLOGGED_MATERIALS Change type from List<Material> to Set<Material>

--- src/main/java/ovh/snipundercover/darkchallenge/util/Constants.java
+++ src/main/java/ovh/snipundercover/darkchallenge/util/Constants.java
@@ -2,13 +2,15 @@ package ovh.snipundercover.darkchallenge.util;
 
 import org.bukkit.Material;
 
-import java.util.List;
+import java.util.EnumSet;
+import java.util.Set;
 
 public final class Constants {
 	private Constants() {}
 	
-	public static final int            TICKS_PER_SECOND      = 20;
-	public static final List<Material> WATERLOGGED_MATERIALS = List.of(
+	public static final int TICKS_PER_SECOND = 20;
+	
+	public static final Set<Material> WATERLOGGED_MATERIALS = EnumSet.of(
 			Material.BUBBLE_COLUMN,
 			Material.KELP,
 			Material.KELP_PLANT,
@@ -16,5 +18,6 @@ public final class Constants {
 			Material.TALL_SEAGRASS,
 			Material.WATER
 	);
+	
 	public static final byte MAX_SAFE_SKY_LIGHT = 11;
 }
